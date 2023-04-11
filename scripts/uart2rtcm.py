45d980812aee3358c971d9d257a620c143d827a9
Mushfiqur Rahman <realmrahman.19@gmail.com>
2023-04-11 10:52:04 +0600

    Resolved debug parameter conflict

--- scripts/uart2rtcm.py
+++ scripts/uart2rtcm.py
@@ -12,18 +12,20 @@ if __name__ == "__main__":
 
     rtcm_topic = rospy.Publisher("rtcm", Message, queue_size=10)
 
-    serial_port = rospy.get_param("port", "/dev/ttyACM0")
+    serial_port = rospy.get_param("~port", "/dev/ttyACM0")
+    rospy.loginfo("Using serial port: " + serial_port)
     ubx = serial.Serial(serial_port, 9600, timeout=1)
 
     rtcm_reader = pyubx2.UBXReader(ubx, protfilter=4)
 
     while not rospy.is_shutdown():
         (raw_data, parsed_data) = rtcm_reader.read()
-        
-        msg = Message()
 
+        msg = Message()
         msg.message = raw_data
         msg.header.seq += 1
         msg.header.stamp = rospy.get_rostime()
 
+        rospy.logdebug("Published " + str(parsed_data.identity) + " Message, Seq: " + str(msg.header.seq))
+        
         rtcm_topic.publish(msg)
